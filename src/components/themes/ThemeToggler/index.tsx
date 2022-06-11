b3b6d23850c604e5480a989c4fa8324f405602b9
Tam Vo <tamvo30@gmail.com>
2022-06-10 22:43:18 -0700

    useCallback to ThemeToggler

--- src/components/themes/ThemeToggler/index.tsx
+++ src/components/themes/ThemeToggler/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { ThemeModeContext } from '@/utils/hooks/useDarkMode';
 import { IconButton } from './styled';
@@ -9,9 +9,9 @@ const ThemeToggler = () => {
   const themeMode = useContext(ThemeModeContext);
   const theme = useTheme();
 
-  const ColorThemeIcon = function () {
+  const ColorThemeIcon = useCallback(() => {
     return theme.palette.mode === 'dark' ? <Brightness4Icon sx={{ color: '#fff' }} /> : <Brightness7Icon sx={{ color: '#fff' }} />;
-  };
+  }, []);
 
   return (
     <IconButton onClick={themeMode.toggleThemeMode}>
