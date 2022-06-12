0e3f03c745a8f2b9089c3069502e5dc154da0c26
Tam Vo <tamvo30@gmail.com>
2022-06-11 21:22:08 -0700

    fixed toggle icon height

--- src/components/themes/ThemeToggler/styled.tsx
+++ src/components/themes/ThemeToggler/styled.tsx
@@ -1,8 +1,9 @@
 import { styled } from '@mui/material/styles';
 import MuiIconButton from '@mui/material/IconButton';
 
-
 export const IconButton = styled(MuiIconButton)`
-  margin-left: 8;
-  margin-right: 8;
+  margin-left: 8px;
+  margin-right: 8px;
+  width: 40px;
+  height: 40px;
 `;
